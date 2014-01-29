18c7e65a535dc7f5d347db18b8ec4bda5724e7f7
birmjin.in <yinbirmjin@gmail.com>
2014-01-29 09:28:20 +0900

    fix runtime error

--- wadler_Chap2.hs
+++ wadler_Chap2.hs
@@ -1,15 +1,13 @@
 --ex.2.4.2
 import Data.List
 
-integers = 0:[ y | x<- [1..], y <- [x, -x]]
-
 splitCandidates:: Int -> [(Int, Int)]
-splitCandidates x = [(y, z) | z <- [-5..5], y <- integers, x == 10 * y + z]
+splitCandidates x = [(y, z) | y <- [-5..5], z <- [-(abs(x) `div` 10)-1..(abs(x) `div` 10)+1], x == y + 10 * z]
 
 split x =
-	if (abs . snd . head $ candidate) == 5 then
-		if (fst . head $ candidate) < (fst .last $ candidate) then
+	if length candidate == 5 then
+		if (snd . head $ candidate) < (snd .last $ candidate) then
 			head candidate
 		else	last candidate
 	else	head candidate
-	where candidate = splitCandidate x
+	where candidate = splitCandidates x
