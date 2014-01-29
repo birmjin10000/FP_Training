3497eeee063554339a72c09cb6940b8981d6f00b
birmjin.in <yinbirmjin@gmail.com>
2014-01-29 09:33:39 +0900

    spec. out bug fix

--- wadler_Chap2.hs
+++ wadler_Chap2.hs
@@ -5,8 +5,8 @@ splitCandidates:: Int -> [(Int, Int)]
 splitCandidates x = [(y, z) | y <- [-5..5], z <- [-(abs(x) `div` 10)-1..(abs(x) `div` 10)+1], x == y + 10 * z]
 
 split x =
-	if length candidate == 5 then
-		if (snd . head $ candidate) < (snd .last $ candidate) then
+	if length candidate > 1 then
+		if (abs . snd . head $ candidate) < (abs . snd .last $ candidate) then
 			head candidate
 		else	last candidate
 	else	head candidate
