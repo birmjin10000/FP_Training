6b9fb9b9fad861fd797be3a62d274235293837df
U-lg-PC\lg <lg@lg-PC.(none)>
2014-02-01 08:18:12 +0900

    ex.2.4.1 added

--- wadler_Chap2.hs
+++ wadler_Chap2.hs
@@ -1,6 +1,17 @@
---ex.2.4.2
 import Data.List
 
+--ex.2.4.1
+age::(Int, Int, Int) -> (Int, Int, Int) -> Int
+age birthday today =
+	if (length diff == 3 || (length diff == 2 && diff!!1 > 0)) then diff!!0
+	else	max (diff!!0 - 1) 0
+	where diff = takeWhile (>=0) $ reverse (zipWith (-) (tripleToList today) (tripleToList birthday))
+
+tripleToList::(Int, Int, Int) -> [Int]
+tripleToList (a, b, c) = a:b:c:[]
+
+
+--ex.2.4.2
 splitCandidates:: Int -> [(Int, Int)]
 splitCandidates x = [(y, z) | y <- [-5..5], z <- [-(abs(x) `div` 10)-1..(abs(x) `div` 10)+1], x == y + 10 * z]
 
